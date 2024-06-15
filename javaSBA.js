98ce01d3b50b88f94fcf4013f193d0e3ef6364fe
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-06-15 17:30:00 -0400

    Removed initial comment

--- javaSBA.js
+++ javaSBA.js
@@ -1,33 +1,3 @@
-//   {
-//     id: number,
-//     name: string,
-//   },
-//   {
-//     id: number,
-//     name: string,
-//     // the ID of the course the assignment group belongs to
-//     course_id: number,
-//     // the percentage weight of the entire assignment group
-//     group_weight: number,
-//     assignments: [AssignmentInfo],
-//   },
-//   {
-//     id: number,
-//     name: string,
-//     // the due date for the assignment
-//     due_at: "12-24-2023", //date string
-//     // the maximum points possible for the assignment
-//     points_possible: number,
-//   },
-//   {
-//     learner_id: number,
-//     assignment_id: number,
-//     submission: {
-//       submitted_at: "12-24-2023", date string
-//       score: number,
-//     },
-//   },
-
 // The provided course information.
 const CourseInfo = {
   id: 451,
