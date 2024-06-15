9196156d604b2b31142b8876425eeeaed3b98055
Zachary Kuchar <25809090+kucharze@users.noreply.github.com>
2024-06-14 20:55:29 -0400

    Changes to data structure, added initial comment for reference

--- javaSBA.js
+++ javaSBA.js
@@ -1,11 +1,43 @@
+// let objs = [
+//   {
+//     id: number,
+//     name: string,
+//   },
+//   {
+//     id: number,
+//     name: string,
+//     // the ID of the course the assignment group belongs to
+//     course_id: number,
+//     // the percentage weight of the entire assignment group
+//     group_weight: number,
+//     assignments: [AssignmentInfo],
+//   },
+//   {
+//     id: number,
+//     name: string,
+//     // the due date for the assignment
+//     due_at: "12-24-2023", //date string
+//     // the maximum points possible for the assignment
+//     points_possible: number,
+//   },
+//   {
+//     learner_id: number,
+//     assignment_id: number,
+//     submission: {
+//       submitted_at: "12-24-2023", date string
+//       score: number,
+//     },
+//   },
+// ];
+
 let objs = [
   {
-    id: number,
-    name: string,
+    id: 100,
+    name: "Zack",
   },
   {
-    id: number,
-    name: string,
+    id: 101,
+    name: "Joe",
     // the ID of the course the assignment group belongs to
     course_id: number,
     // the percentage weight of the entire assignment group
@@ -13,8 +45,8 @@ let objs = [
     assignments: [AssignmentInfo],
   },
   {
-    id: number,
-    name: string,
+    id: 95,
+    name: "Chris",
     // the due date for the assignment
     due_at: "12-24-2023", //date string
     // the maximum points possible for the assignment
